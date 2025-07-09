fd3884a2fce1f59cee6399d1e4f89da5c5ea7a0f
Mirsadc1971 <mirsadc1971@icloud.com>
2025-07-09 18:08:43 -0500

    Update Legal Disclaimers with IDFPR License Number

---
+++ src/pages/LegalDisclaimers.tsx
@@ -0,0 +1,8 @@
+@@ .. @@
+                <p className="text-gray-300 text-sm leading-relaxed">
+                  Manage369 is a licensed property management company in the State of Illinois. 
+-                  License #: [License Number]. All property management activities are conducted 
++                  License #: 291.000211. All property management activities are conducted 
+                  in accordance with Illinois Department of Financial and Professional Regulation 
+                  (IDFPR) requirements and Community Associations Institute (CAI) standards.
+                </p>
