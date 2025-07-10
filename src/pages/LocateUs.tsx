1198377d17c941cf7830e412106dc3b58ff58ea6
Mirsadc1971 <mirsadc1971@icloud.com>
2025-07-09 23:10:08 -0500

    Fix Google Maps iframe

--- src/pages/LocateUs.tsx
+++ src/pages/LocateUs.tsx
@@ -564,7 +564,7 @@ const LocateUs = () => {
             <div className="bg-gray-100 p-4 rounded-xl">
               <div className="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden">
                 <iframe 
-                  src="https://maps.app.goo.gl/gPy3kMGiv4bLnh4r7"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.8!2d-87.6298!3d41.8781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDUyJzQxLjIiTiA4N8KwMzcnNDcuMyJX!5e0!3m2!1sen!2sus!4v1642000000000!5m2!1sen!2sus"
                   width="100%" 
                   height="450" 
                   style={{ border: 0 }}
@@ -577,7 +577,7 @@ const LocateUs = () => {
               </div>
               <div className="text-center mt-4">
                 <a 
-                  href="https://maps.app.goo.gl/gPy3kMGiv4bLnh4r7" 
+                  href="https://www.google.com/maps/search/Chicago+IL/@41.8781,-87.6298,15z" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="btn-primary inline-flex items-center"
