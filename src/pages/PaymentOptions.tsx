07e3eddfeba785811dd14227062b0072639a3749
Mirsadc1971 <mirsadc1971@icloud.com>
2025-07-16 18:28:34 -0500

    Fix unclosed JSX tag in PaymentOptions.tsx

--- src/pages/PaymentOptions.tsx
+++ src/pages/PaymentOptions.tsx
@@ -403,7 +403,6 @@ const PaymentOptions = () => {
             >
               Access Online Portal
             </a>
-            <a href="tel:312-402-7002" className="btn-outline border-white text-white hover:bg-white hover:text-primary-500 text-lg px-8 py-4">
             <a href="tel:847-834-4131" className="btn-outline border-white text-white hover:bg-white hover:text-primary-500 text-lg px-8 py-4">
               Call (847) 834-4131
             </a>
